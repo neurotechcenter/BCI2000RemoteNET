13ebe4237777c338eb44910a45da82c973ac60c7
Ty <tytbu@tytbutler.com>
2023-08-31 13:04:53 -0500

    Fixes to response handling

--- BCI2000CommandException.cs
+++ BCI2000CommandException.cs
@@ -2,7 +2,7 @@ using System;
 /**
  *  Exception indicating that a command sent to BCI2000 has failed
  */
-class BCI2000CommandException : Exception
+internal class BCI2000CommandException : Exception
 {
-	public BCI2000CommandException(string msg) : base(msg) { }
+	internal BCI2000CommandException(string msg) : base(msg) { }
 }
--- BCI2000Connection.cs
+++ BCI2000Connection.cs
@@ -12,6 +12,7 @@
 
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Net.Sockets;
@@ -31,6 +32,7 @@ namespace BCI2000RemoteNET
 
 
         private TcpClient tcp;
+        private NetworkStream operator_stream;
 
         //response stuff
         private const string ReadlineTag = "\\AwaitingInput:";
@@ -69,8 +71,6 @@ namespace BCI2000RemoteNET
         public string TelnetIp { get; set; }
         public Int32 TelnetPort { get; set; }
         public string OperatorPath { get; set; }
-
-
 
 
         private string result; //three properties for results and logging
@@ -225,15 +225,17 @@ namespace BCI2000RemoteNET
             }
             catch (SocketException ex)
             {
-                Result = "Failed to connect to " + TelnetIp + ":" + TelnetPort + ", " + ex.Message;
+                throw new BCI2000ConnectionException("Failed to connect to " + TelnetIp + ":" + TelnetPort + ", " + ex.Message);
                 success = false;
             }
 
             if (!success && (String.IsNullOrEmpty(OperatorPath)))
             {
-                Result = "Failed to connect to " + TelnetIp + ":" + TelnetPort;
+                throw new BCI2000ConnectionException("Failed to connect to " + TelnetIp + ":" + TelnetPort);
                 return false;
             }
+
+            operator_stream = new NetworkStream(tcp.Client);
 
             if (!success) //tcp has not connected to running operator, so it will try to open a new operator and connect
             {
@@ -252,8 +254,9 @@ namespace BCI2000RemoteNET
 
                     System.Diagnostics.Process.Start(OperatorPath, arguments.ToString());
                 }
-                catch (InvalidOperationException)
+                catch (InvalidOperationException ex)
                 {
+                    throw ex;
                     Result = "Failed to run operator at " + OperatorPath;
                     return false;
                 }
@@ -264,6 +267,7 @@ namespace BCI2000RemoteNET
                 catch (SocketException ex)
                 {
                     Result = "Failed to connect to operator at 127.0.0.1:" + TelnetPort + ", " + ex.Message;
+                    throw ex;
                     return false;
                 }
 
@@ -280,6 +284,10 @@ namespace BCI2000RemoteNET
 
             return true;
         }
+        public void WaitForConnected()
+        {
+            Execute("Wait for Connected");
+        }
         public bool Execute(string command)
         {
             int unused = 0;
@@ -294,90 +302,87 @@ namespace BCI2000RemoteNET
                 return false;
             }
 
-            string responseUnprocessed;
             Sending = command;
             try
             {
                 tcp.Client.Send(stringToBytes(command + "\r\n"));
-                Byte[] responseData = new Byte[1024];
-                tcp.Client.Receive(responseData);
-                responseUnprocessed = bytesToString(responseData);
             }
             catch (SocketException ex)
             {
                 Result = "SocketException: " + ex + ", socket error code " + ex.SocketErrorCode;
                 return false;
             }
-            Received = responseUnprocessed;
-            return ProcessResponse(responseUnprocessed, ref outCode);
+           
+            return ProcessResponse(ref outCode);
         }
 
-        public bool ProcessResponse(string responseUnprocessed, ref int outCode)
+        public bool ProcessResponse(ref int outCode)
         {
             Response = "";
             byte[] buffer = new byte[1024];
-            while (Connected() && (!(responseUnprocessed.IndexOf(Prompt, StringComparison.OrdinalIgnoreCase) >= 0)) || tcp.Client.Available > 0)
-            {
-                if (responseUnprocessed.Contains(ReadlineTag))
+            StringBuilder line = new StringBuilder();
+            while (Connected() && (!(line.ToString().Equals(Prompt))) || operator_stream.DataAvailable)
                 {
-                    string input = "";
-                    if (OnInput())
+                char c = (char) operator_stream.ReadByte();
+                if (c == '\n' || c == -1) // -1 means all data has been read
+                {
+                    string responseUnprocessed = line.ToString();
+                    if (responseUnprocessed.Contains(ReadlineTag))
                     {
-                        tcp.Client.Send(stringToBytes(input + "\r\n"));
-                        Byte[] recvBuf = new byte[1024];
-                        tcp.Client.Receive(recvBuf);
-                        if (!bytesToString(recvBuf).Contains(AckTag))
+                        string input = "";
+                        if (OnInput())
                         {
-                            Result = "Did not receive input acknowledgement";
+                            tcp.Client.Send(stringToBytes(input + "\r\n"));
+                            Byte[] recvBuf = new byte[1024];
+                            tcp.Client.Receive(recvBuf);
+                            if (!bytesToString(recvBuf).Contains(AckTag))
+                            {
+                                throw new BCI2000ConnectionException("Did not receive input acknowledgement");
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            throw new BCI2000ConnectionException("Could not handle request for input: Override BCI2000Connection.OnInput to handle input");
                             return false;
                         }
                     }
+                    else if (responseUnprocessed.Contains(ExitCodeTag))
+                    {
+                        outCode = responseUnprocessed.Length;
+                    }
+                    else if (responseUnprocessed.Contains(TerminationTag))
+                    {
+                        tcp.Client.Close();
+                        tcp.Close();
+                        TerminateOperator = false;
+                        return true;
+                    }
                     else
                     {
-                        Result = "Could not handle request for input: Override BCI2000Connection.OnInput to handle input";
-                        return false;
+                        if (!OnOutput(responseUnprocessed))
+                        {
+                            Response = responseUnprocessed;
+                        }
+                        if (responseUnprocessed.IndexOf("true", StringComparison.OrdinalIgnoreCase) >= 0)//this is rewritten from the original code, but changed so that 1 reflects true and 0 reflects false
+                            outCode = 1;
+                        else if (responseUnprocessed.IndexOf("false", StringComparison.OrdinalIgnoreCase) >= 0)
+                            outCode = 0;
+                        else if (String.IsNullOrWhiteSpace(responseUnprocessed))
+                            outCode = 1;
+                        else
+                            outCode = -1;
                     }
+                    line.Clear();
                 }
-                else if (responseUnprocessed.Contains(ExitCodeTag))
+                if (c != '\r')
                 {
-                    outCode = responseUnprocessed.Length;
+                    line.Append(c);
                 }
-                else if (responseUnprocessed.Contains(TerminationTag))
-                {
-                    tcp.Client.Close();
-                    tcp.Close();
-                    TerminateOperator = false;
-                    return true;
-                }
-                else
-                {
-                    if (!OnOutput(responseUnprocessed))
-                    {
-                        Response = responseUnprocessed;
-                    }
-                    if (responseUnprocessed.IndexOf("true", StringComparison.OrdinalIgnoreCase) >= 0)//this is rewritten from the original code, but changed so that 1 reflects true and 0 reflects false
-                        outCode = 1;
-                    else if (responseUnprocessed.IndexOf("false", StringComparison.OrdinalIgnoreCase) >= 0)
-                        outCode = 0;
-                    else if (String.IsNullOrWhiteSpace(responseUnprocessed))
-                        outCode = 1;
-                    else
-                        outCode = -1;
-                }
-                Array.Clear(buffer, 0, 1024);
-                tcp.Client.Receive(buffer);
-                responseUnprocessed = responseUnprocessed + bytesToString(buffer);
             }
             if (!Connected())
             {
-                Result = "Lost Connection to BCI2000";
-                return false;
-            }
-            if (responseUnprocessed.IndexOf(Prompt, StringComparison.OrdinalIgnoreCase) >= 0)
-            {
-                Response = responseUnprocessed;
-                outCode = 1;
-                return true;
+                throw new BCI2000ConnectionException("Lost Connection to BCI2000");
             }
             return true;
         }
