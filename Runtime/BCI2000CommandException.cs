ae6ca5b28068a5cdd25ce4d4212ab6f5d919927c
Twig <littleb88888888@gmail.com>
2025-02-14 14:59:14 -0700

    Moved exception classes - migrated custom exceptions to new script under modified namespace

--- Runtime/BCI2000CommandException.cs
+++
@@ -1,8 +0,0 @@
-using System;
-/**
- *  Exception indicating that a command sent to BCI2000 has failed
- */
-internal class BCI2000CommandException : Exception
-{
-	internal BCI2000CommandException(string msg) : base(msg) { }
-}
---
+++ Runtime/BCI2000Exceptions.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Bci2000
+{
+/**
+    *  Exception indicating that a command sent to BCI2000 has failed
+    */
+    internal class BCI2000CommandException : Exception
+    {
+        internal BCI2000CommandException(string msg) : base(msg) { }
+    }
+
+    internal class BCI2000ConnectionException : Exception 
+    {
+        internal BCI2000ConnectionException(string message) : base(message) { }
+    }
+}
